fde2db67b2685f7921540fba08d4c59f17cfbb70
chowdary709 <chandrahari321@gmail.com>
2024-05-22 13:22:23 +0530

    update

--- state.tf
+++ state.tf
@@ -1,5 +1,7 @@
-backend "s3" {
-  bucket = "terraform-709"
-  key    = "parameter-store/parameter/terraform.tfstate"
-  region = "us-east-1"
+terraform {
+  backend "s3" {
+    bucket = "terraform-709"
+    key    = "parameter-store/parameter/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
