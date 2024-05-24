b6e3f7344c17e80f24846230dc36c2fe001e0497
chowdary709 <chandrahari321@gmail.com>
2024-05-24 22:05:23 +0530

    update

--- main.tf
+++ main.tf
@@ -17,13 +17,13 @@ variable "parameters" {
     {name = "expense.backend.DB_HOST", value = "mysql.roboshop.internal", type = "String"},                     # backend
     {name = "expense.backend.DB_PASS", value = "ExpenseApp@1", type = "SecureString"},                          # backend
     {name = "expense.mysql.DB_PASS", value = "ExpenseApp@1", type = "SecureString"},                             # mysql
-      
+
     ###dev
     # rds terraform dev environment
     {name = "dev.rds.master_username", value = "admin1", type = "String"},
     {name = "dev.rds.master_password", value = "Expense12345", type = "SecureString"},
     #rds end point
-    {name = "dev.rds.endpoint", value = "dev-mysql.cluster-cinvurdqvuv4.us-east-1.rds.amazonaws.com", type = "String"}
+    {name = "dev.rds.endpoint", value = "dev-mysql.cluster-cinvurdqvuv4.us-east-1.rds.amazonaws.com", type = "String"},
 
     ###prod
     # rds terraform prod environment
