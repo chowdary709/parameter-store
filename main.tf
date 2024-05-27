1b9b46ed358bb189042843003cd7405797d64d56
chowdary709 <chandrahari321@gmail.com>
2024-05-27 18:41:16 +0530

    update

--- main.tf
+++ main.tf
@@ -12,9 +12,9 @@ resource "aws_ssm_parameter" "params" {
 variable "parameters" {
   default = [
     ### expense-ansible-parameters
-#     {name = "expense.frontend.backend_url", value = "http://backend.roboshop.internal:8080/", type = "String"}, # frontend
-#     {name = "expense.frontend.backend_url", value = "http://backend-dev.roboshop.internal/", type = "String"}, # frontend
-    {name = "expense.backend.DB_HOST", value = "mysql.roboshop.internal", type = "String"},                     # backend
+#     {name = "expense.frontend.backend_url", value = "http://backend.chowdary.cloud:8080/", type = "String"}, # frontend
+#     {name = "expense.frontend.backend_url", value = "http://backend-dev.chowdary.cloud/", type = "String"}, # frontend
+    {name = "expense.backend.DB_HOST", value = "mysql.chowdary.cloud", type = "String"},                     # backend
     {name = "expense.backend.DB_PASS", value = "ExpenseApp@1", type = "SecureString"},                          # backend
     {name = "expense.mysql.DB_PASS", value = "ExpenseApp@1", type = "SecureString"},                             # mysql
 
@@ -24,7 +24,7 @@ variable "parameters" {
     {name = "dev.rds.master_password", value = "Expense12345", type = "SecureString"},
     #rds end point
     {name = "dev.rds.endpoint", value = "dev-mysql.cluster-cinvurdqvuv4.us-east-1.rds.amazonaws.com", type = "String"},
-    {name = "dev.expense.frontend.backend_url", value = "http://backend-dev.roboshop.internal/", type = "String"},
+    {name = "dev.expense.frontend.backend_url", value = "http://backend-dev.chowdary.cloud/", type = "String"},
 
 
     ###prod
@@ -33,7 +33,7 @@ variable "parameters" {
     {name = "prod.rds.master_password", value = "Expense12345", type = "SecureString"},
     #rds end point
     {name = "prod.rds.endpoint", value = "prod-mysql.cluster-cinvurdqvuv4.us-east-1.rds.amazonaws.com", type = "String"},
-    {name = "prod.expense.frontend.backend_url", value = "http://backend-prod.roboshop.internal/", type = "String"}
+    {name = "prod.expense.frontend.backend_url", value = "http://backend-prod.chowdary.cloud/", type = "String"}
   ]
 }
 
@@ -48,12 +48,12 @@ variable "parameters" {
 #   default = [
 #     {name = "prod.rds.master_password", value = "expenseprod1234", type = "SecureString"},
 #     {name = "prod.rds.master_username", value = "mysqlrdsadmin", type = "String"},
-#     {name = "prod.expense.frontend.backend_url", value = "http://backend-prod.roboshop.internal/", type = "String"},
+#     {name = "prod.expense.frontend.backend_url", value = "http://backend-prod.chowdary.cloud/", type = "String"},
 #     {name = "prod.rds.endpoint", value = "prod-mysql.cluster-cbvsbeoyxek4.us-east-1.rds.amazonaws.com", type = "String"},
 #
 #     ## Dev env
 #     {name = "dev.rds.master_username", value = "admin1", type = "String"},
 #     {name = "dev.rds.master_password", value = "Expense12345", type = "SecureString"},
-#     {name = "dev.expense.frontend.backend_url", value = "http://backend-dev.roboshop.internal/", type = "String"}
+#     {name = "dev.expense.frontend.backend_url", value = "http://backend-dev.chowdary.cloud/", type = "String"}
 #   ]
 # }
