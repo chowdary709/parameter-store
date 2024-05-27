76c328e0537722776a354361776da957a6a82f35
chowdary709 <chandrahari321@gmail.com>
2024-05-27 12:06:31 +0530

    update

--- main.tf
+++ main.tf
@@ -1,3 +1,42 @@
+# resource "aws_ssm_parameter" "params" {
+#   count = length(var.parameters)
+#   name = var.parameters[count.index].name
+#   type = var.parameters[count.index].type
+#   value = var.parameters[count.index].value
+# }
+#
+#
+# # String
+# #  SecureString
+#
+# variable "parameters" {
+#   default = [
+#     ### expense-ansible-parameters
+# #     {name = "expense.frontend.backend_url", value = "http://backend.roboshop.internal:8080/", type = "String"}, # frontend
+# #     {name = "expense.frontend.backend_url", value = "http://backend-dev.roboshop.internal/", type = "String"}, # frontend
+#     {name = "expense.backend.DB_HOST", value = "mysql.roboshop.internal", type = "String"},                     # backend
+#     {name = "expense.backend.DB_PASS", value = "ExpenseApp@1", type = "SecureString"},                          # backend
+#     {name = "expense.mysql.DB_PASS", value = "ExpenseApp@1", type = "SecureString"},                             # mysql
+#
+#     ###dev
+#     # rds terraform dev environment
+#     {name = "dev.rds.master_username", value = "admin1", type = "String"},
+#     {name = "dev.rds.master_password", value = "Expense12345", type = "SecureString"},
+#     #rds end point
+#     {name = "dev.rds.endpoint", value = "dev-mysql.cluster-cinvurdqvuv4.us-east-1.rds.amazonaws.com", type = "String"},
+#     {name = "dev.expense.frontend.backend_url", value = "http://backend-dev.roboshop.internal/", type = "String"},
+#
+#
+#     ###prod
+#     # rds terraform prod environment
+#     {name = "prod.rds.master_username", value = "admin1", type = "String"},
+#     {name = "prod.rds.master_password", value = "Expense12345", type = "SecureString"},
+#     #rds end point
+#     {name = "prod.rds.endpoint", value = "prod-mysql.cluster-cinvurdqvuv4.us-east-1.rds.amazonaws.com", type = "String"},
+#     {name = "prod.expense.frontend.backend_url", value = "http://backend-prod.roboshop.internal/", type = "String"}
+#   ]
+# }
+#
 resource "aws_ssm_parameter" "params" {
   count = length(var.parameters)
   name = var.parameters[count.index].name
@@ -5,35 +44,14 @@ resource "aws_ssm_parameter" "params" {
   value = var.parameters[count.index].value
 }
 
-
-# String
-#  SecureString
-
 variable "parameters" {
   default = [
-    ### expense-ansible-parameters
-#     {name = "expense.frontend.backend_url", value = "http://backend.roboshop.internal:8080/", type = "String"}, # frontend
-#     {name = "expense.frontend.backend_url", value = "http://backend-dev.roboshop.internal/", type = "String"}, # frontend
-    {name = "expense.backend.DB_HOST", value = "mysql.roboshop.internal", type = "String"},                     # backend
-    {name = "expense.backend.DB_PASS", value = "ExpenseApp@1", type = "SecureString"},                          # backend
-    {name = "expense.mysql.DB_PASS", value = "ExpenseApp@1", type = "SecureString"},                             # mysql
+    {name = "prod.rds.master_password", value = "expenseprod1234", type = "SecureString"},
+    {name = "prod.rds.master_username", value = "mysqlrdsadmin", type = "String"},
+    {name = "prod.expense.frontend.backend_url", value = "http://backend-prod.rdevopsb73.online/", type = "String"},
+    {name = "prod.rds.endpoint", value = "prod-mysql.cluster-cbvsbeoyxek4.us-east-1.rds.amazonaws.com", type = "String"},
 
-    ###dev
-    # rds terraform dev environment
-    {name = "dev.rds.master_username", value = "admin1", type = "String"},
-    {name = "dev.rds.master_password", value = "Expense12345", type = "SecureString"},
-    #rds end point
-    {name = "dev.rds.endpoint", value = "dev-mysql.cluster-cinvurdqvuv4.us-east-1.rds.amazonaws.com", type = "String"},
-    {name = "dev.expense.frontend.backend_url", value = "http://backend-dev.roboshop.internal/", type = "String"},
-
-
-    ###prod
-    # rds terraform prod environment
-    {name = "prod.rds.master_username", value = "admin1", type = "String"},
-    {name = "prod.rds.master_password", value = "Expense12345", type = "SecureString"},
-    #rds end point
-    {name = "prod.rds.endpoint", value = "prod-mysql.cluster-cinvurdqvuv4.us-east-1.rds.amazonaws.com", type = "String"},
-    {name = "prod.expense.frontend.backend_url", value = "http://backend-prod.roboshop.internal/", type = "String"}
+    ## Dev env
+    {name = "dev.expense.frontend.backend_url", value = "http://backend-dev.rdevopsb73.online/", type = "String"}
   ]
 }
-
