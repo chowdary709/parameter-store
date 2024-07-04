be580c8a1ae2386407b32364433d134ce2aad507
chowdary709 <chandrahari321@gmail.com>
2024-07-04 09:28:18 +0530

    automated commit #3163

--- sg/sg.tf
+++ sg/sg.tf
@@ -13,7 +13,7 @@ resource "aws_security_group" "allow_all" {
   egress {
     description = "Allow all outbound traffic"
     from_port   = 0
-    to_port     = 0
+    to_port     = 65535
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
